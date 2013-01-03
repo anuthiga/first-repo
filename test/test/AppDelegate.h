505180e151874f8bf05b0c82c5a2d446288c773b
Anuthiga Sriskanthan <anuthigasriskanthan@Hasala-Sandaruwans-MacBook.local>
2013-01-03 14:58:14 +0530

    Testing

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Anuthiga Sriskanthan on 1/3/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
